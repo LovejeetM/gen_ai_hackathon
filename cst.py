33403efa5dfe11941d205945186cc3f03f26ec68
LovejeetM <lovejeetmatharu54@gmail.com>
2025-09-16 11:17:39 +0530

    GUI

--- cst.py
+++ cst.py
@@ -1,9 +1,17 @@
 import customtkinter
 import threading
 from custom import initial_greeting, chat_gen
+from PIL import Image
+import io
+import base64
 
 chat_history = []
 is_button_active_global = False
+
+BACK_ARROW_B64 = b"iVBORw0KGgoAAAANSUhEUgAAADIAAAAyCAYAAAAeP4ixAAAACXBIWXMAAAsTAAALEwEAmpwYAAAB10lEQVR4nO3XPY9MURzA4bNIWLEKEhIaiUq8RLOFhsLLB0AkohGFaDQSoaRCoaCi2YR6s6g0KDReQr8KIgqFRCLeVpb1yM2eSS6ZmZ3NPTN3jtznA5x7f5m55/xPCI1Go9FoNBr/N2zGSzwMucIefBCFHOEkZlsR2YVgOSbKAdmFYAOetovIJgS78L5TRBYhOIbv3SKGOgRLcXmhgKEOwdribLA4nzCNx5jEBRzA6roituONdObwHKexZlARh/BF//zAbWzqV8AILuK3wZjBFYyljFiFKfV4hR2pQp6p1zccSRHS8bQeoF/FeVU1ZAx3hiTmcIqP/VzcKuv0FVsrxQxo++3FNFamOhBf1xxzqXJIhRElpdkkf7EYswzXFvHwUayL9/hxHMRZ3Ig74183yh5MJgkpBZ2Io0VXPawzGu/5V/Guh5A5bBvqi1XcJffi/gJj0c2kIfHhG7tNARXWHY/TcTufixEqbcn8Q1fgVsqQApbgPH62Wbr6+NIJzsSTOElIC/bFQ7FsIvQT9uNjypACdscxv6X4NkdCP8Wt9gUeJF73+D+/ys6QK9wthZwKucKW0hB7PeQM92LIo5AzHI0hb0POsD6GzITcmR9jntT9Ho1GI9TjD22H/Nq+o1wxAAAAAElFTkSuQmCC"
+
+MIC_B64= b"iVBORw0KGgoAAAANSUhEUgAAADAAAAAwCAYAAABXAvmHAAAACXBIWXMAAAsTAAALEwEAmpwYAAACEElEQVR4nO2YP0scQRiHB3ORCBrbYE4tIiFGP0j+gN/CWFpai4WxUE9N/CAmnQmISRMsPSslFp5aaOOdiYXKIy8ZYXmZ5HZv59wxzAMHx87cb99nmX13b4yJRCJeAUaBClAFzu2nao+NmlABOoGPwDV/5wpYkbkmwOK/kp4vQUnw58pnZdkEtOZlaWRFfjMSgsASrbMQgsBODoHtEAQaOQTqIQjkwhRNFCiaKFA0UaBookDRBC9AkxP6FsC34P8g0FCZPWq8nqP+M5X12PvLHrCnQp+363UaeKHGd30IfFehr9X4Yg6BeZX1Vo1/8yEguwhJPqjxkRx/KV+qrFU1Z8mHwBsVegCUmkimoaIySkBNzXnlQ6AL+KWCxx3bKrJVkpZ14KHKmFBzZEOsK7eADZddtSSHjm4kEstNltOlzdLF9wLH/7o/8gr0ARfqBGtAh2Ou3BML0mFsC27Y7/N6zQuSAXxW2b+BJ8YnwIzjiq64JDJkdkhTcOROey0+cZPplip8kgdQC3mybPSVFzZ1k/Ap0Q8cOU4qx96lObG9EBOONX+bU25L8YkChoB93NRsL5fWOwx028+wfUitOlrlLT+BZ20tPiFRBrbwxw/g6Z0Ur5bCpO3XrXIBTAEP7rR4JTIIzAGnGQo/Ad4DAyYU7BN7DJgFNhxFb9gxmfPIhA4Kc98gChRMFCiaey8QMem4ATKfZRavGuKEAAAAAElFTkSuQmCC"
+
 
 customtkinter.set_appearance_mode("dark")
 
@@ -79,9 +87,29 @@ def show_language_selection():
         btn.grid(row=i//2, column=i%2, padx=35, pady=35) #btn.grid(row=i//2, column=i%2, padx=15, pady=15)
 
 def show_chat_interface():
-    global chat_display, record_button
+    global chat_display, record_button, mic_icon
     for widget in root.winfo_children():
         widget.destroy()
+
+    img_data = base64.b64decode(BACK_ARROW_B64)
+    img = Image.open(io.BytesIO(img_data))
+    back_icon = customtkinter.CTkImage(light_image=img, dark_image=img, size=(40, 40))
+    back_button = customtkinter.CTkButton(
+        root,
+        text="",
+        image=back_icon,
+        command=show_language_selection,
+        width=50,
+        height=60,
+        corner_radius=20,
+        fg_color="#D35B58",
+        hover_color="#C77C78"
+    )
+    back_button.place(relx=1.0, x=-20, y=15, anchor="ne")
+
+    img_data = base64.b64decode(MIC_B64)
+    img = Image.open(io.BytesIO(img_data))
+    mic_icon = customtkinter.CTkImage(light_image=img, dark_image=img, size=(60, 60))    ### BUTTON
     greeting = initial_greeting(language_for_agent)
     chat_history.append([None, greeting])
     chat_display = customtkinter.CTkFrame(root, fg_color='#1e1f22', corner_radius=15)
@@ -89,16 +117,17 @@ def show_chat_interface():
     display_message(greeting, 'agent')
     record_button = customtkinter.CTkButton(
         root,
-        text="REC",
+        text="",  # REC
+        image=mic_icon,
         font=("Arial Rounded MT Bold", 24, "bold"),   #18
         fg_color='#1a73e8',
         hover_color='#155cba',
         command=toggle_recording,
-        width=160, #120
+        width=100, #120
         height=120,
-        corner_radius=50   #60
+        corner_radius= 50   #60
     )
-    record_button.pack(side='right', padx=(30, 30), pady=20)
+    record_button.pack(side='right', padx=(20, 20), pady=20)  #  pdx = 30, 30    pady = 20
 
 def display_message(message, sender):
     msg_frame = customtkinter.CTkFrame(chat_display, fg_color='transparent', corner_radius=15)  # 15
@@ -120,11 +149,11 @@ def toggle_recording():
     global is_button_active_global
     if not is_button_active_global:
         is_button_active_global = True
-        record_button.configure(text="STOP", fg_color='#155cba')
-        threading.Thread(target=handle_audio_interaction, daemon=True).start()
+        record_button.configure(text="",image=mic_icon, fg_color='#155cba')    #STOP
+        # threading.Thread(target=handle_audio_interaction, daemon=True).start()
     else:
         is_button_active_global = False
-        record_button.configure(text="REC", fg_color='#1a73e8')
+        record_button.configure(text="",image=mic_icon, fg_color='#1a73e8')   #REC
 
 def handle_audio_interaction():
     global is_button_active_global
